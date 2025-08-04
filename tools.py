d6715000e50aedd2b52b04119980c85a71608281
Hannah <Hseppal1@umbc.edu>
2025-08-04 15:52:58 +0000

    fixed_tools_but_actually
    Turns out I don't know cardinal directions?

--- tools.py
+++ tools.py
@@ -13,14 +13,14 @@ def time_from_attr(ds):
     ds = ds.set_coords("date")
     return ds
 
-def open_L2_CLOUD_GPC(path,N=-120,E=-50,S=-170,W=-50):
+def open_L2_CLOUD_GPC(path,N=-50,E=-120,S=-60,W=-170):
     dt = xr.open_datatree(path)
     ds = xr.merge(dt.to_dict().values())
     ds = time_from_attr(ds)
     ds = ds.set_coords(('latitude','longitude'))
     ds = ds[['cloud_bow_droplet_effective_radius','cloud_bow_droplet_effective_variance','cloud_bow_droplet_number_concentration_adiabatic','cloud_bow_liquid_water_path']]
-    lat_mask = np.logical_and(ds.latitude<=W,ds.latitude>=E)
-    lon_mask = np.logical_and(ds.longitude<=N,ds.longitude>=S)
+    lat_mask = np.logical_and(ds.latitude<=N,ds.latitude>=S)
+    lon_mask = np.logical_and(ds.longitude<=E,ds.longitude>=W)
     mask = np.logical_and(lat_mask,lon_mask)
     ds = ds.where(mask,drop=True)
     return(ds)
