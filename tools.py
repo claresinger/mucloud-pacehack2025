2366b2ace97c455403937095caf15168a212fff8
Hannah <Hseppal1@umbc.edu>
2025-08-04 15:32:52 +0000

    Updated_tools
    made bounding box into variables with default values

--- tools.py
+++ tools.py
@@ -10,14 +10,14 @@ def time_from_attr(ds):
     ds = ds.set_coords("date")
     return ds
 
-def open_L2_CLOUD_GPC(path):
+def open_L2_CLOUD_GPC(path,N=-120,E=-50,S=-170,W=-50):
     dt = xr.open_datatree(path)
     ds = xr.merge(dt.to_dict().values())
     ds = time_from_attr(ds)
     ds = ds.set_coords(('latitude','longitude'))
     ds = ds[['cloud_bow_droplet_effective_radius','cloud_bow_droplet_effective_variance','cloud_bow_droplet_number_concentration_adiabatic','cloud_bow_liquid_water_path']]
-    lat_mask = np.logical_and(ds.latitude<=-50,ds.latitude>=-60)
-    lon_mask = np.logical_and(ds.longitude<=-120,ds.longitude>=-170)
+    lat_mask = np.logical_and(ds.latitude<=W,ds.latitude>=E)
+    lon_mask = np.logical_and(ds.longitude<=N,ds.longitude>=S)
     mask = np.logical_and(lat_mask,lon_mask)
     ds = ds.where(mask,drop=True)
     return(ds)
